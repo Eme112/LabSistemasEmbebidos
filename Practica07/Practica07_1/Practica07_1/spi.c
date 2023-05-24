68d8095ae81fb274efd61b579ab5afe37590fbb5
Eme112 <eme_pedraza@hotmail.com>
2023-05-24 00:24:15 -0600

    few modifications

--- Practica07/Practica07_1/Practica07_1/spi.c
+++ Practica07/Practica07_1/Practica07_1/spi.c
@@ -2,6 +2,24 @@
 #include "myprintf.h"
 #include "spi.h"
 
+#define RXBUFSIZE 0x400
+#define LENGTH_R1 0x03
+#define LENGTH_R7 0x07
+
+#define SIZE_SD_CMD 0x06
+#define kCMD00 0x40
+#define kCMD08 0x48
+#define kCMD55 0x77
+#define kCMD41 0x69
+const uint8_t CMD00[SIZE_SD_CMD] ={0x40, 0x00, 0x00, 0x00, 0x00, 0x95};
+const uint8_t CMD08[SIZE_SD_CMD] ={0x48, 0x00, 0x00, 0x01, 0xAA, 0x0F};
+uint8_t CMD17[SIZE_SD_CMD] ={0x51, 0x00, 0x00, 0x00, 0x00, 0x01};
+uint8_t CMD172[SIZE_SD_CMD] ={0x51, 0x00, 0x00, 0x08, 0x00, 0x01};
+const uint8_t CMD18[SIZE_SD_CMD] ={0x52, 0x00, 0x00, 0x00, 0x00, 0x01};
+const uint8_t CMD55[SIZE_SD_CMD] ={0x77, 0x00, 0x00, 0x00, 0x00, 0x65};
+const uint8_t CMD41[SIZE_SD_CMD] = {0x69, 0x40, 0x00, 0x00, 0x00, 0x77};
+uint8_t RxBuffer[RXBUFSIZE];
+
 void spiInit( void ) {
 	/* Switch to 8MHz clock (disable prescaler) */
 	SYSCTRL->OSC8M.bit.PRESC = 0;
@@ -11,7 +29,7 @@ void spiInit( void ) {
 	while( GCLK->STATUS.bit.SYNCBUSY ) { }
 	//structures to configure the SERCOM1 peripheral
 	const SERCOM_SPI_CTRLA_Type ctrla = {
-		.bit.DORD = SERCOM_SPI_CTRLA_DORD, // MSB first
+		.bit.DORD = 0, // MSB first
 		.bit.CPHA = 0, // Mode 0
 		.bit.CPOL = 0,
 		.bit.FORM = 0, // SPI frame
@@ -25,7 +43,6 @@ void spiInit( void ) {
 		.bit.MSSEN = 1, // Manual SC
 		.bit.CHSIZE = 0 // 8-bit
 	};
-
 	SERCOM1->SPI.CTRLB.reg = ctrlb.reg;
 	//Formula to configure the desired baudrate
 	uint32_t br = ( double )( 8000000 / ( 2 * BAUDRATE ) ) - 1 ;
@@ -52,26 +69,79 @@ uint8_t spiSend( uint8_t data ) {
 	uint8_t temp;
 	while(!SERCOM1->SPI.INTFLAG.bit.DRE) { } //wait until buffer is empty
 	SERCOM1->SPI.DATA.reg = SERCOM_SPI_DATA_DATA( data ); //transmit data
-	myprintf( "Data sent \t0x%x\n", data ); //printf the value in putty
 	while( !SERCOM1->SPI.INTFLAG.bit.RXC ) { } //wait until a data is received
 	temp = SERCOM1->SPI.DATA.reg; //read data
 	while( !SERCOM1->SPI.INTFLAG.bit.TXC ) { } //wait until there is no data to transmit
-	myprintf( "Data received \t0x%x\n", temp ); //printf the value in putty
+	//myprintf( "\nR: %x", temp ); //printf the value in putty
 	return temp;
+}
+
+void initCycles(void){
+	uint32_t i;
+	REG_PORT_OUTSET0 = PORT_PA18;
+	for(i=0;i<77;i++)
+	spiSend(0xFF);
+}
+
+uint32_t spiXchg(const uint8_t * send_buff, uint32_t bc, uint8_t * receive_buff ) {
+	uint8_t temp = 0xFF;
+	uint32_t i;
+	uint8_t temp_cmd = send_buff[0];
+	REG_PORT_OUTCLR0 = PORT_PA18;
+	for(i=0; i< bc; i++) {
+		temp = spiSend(*(send_buff++));
+		// myprintf(" %x", temp);
+	}
+	switch(temp_cmd) {
+		case kCMD00 :
+		myprintf("\nR1 = ");
+		for(i=0; i<LENGTH_R1; i++) {
+			temp = spiSend(0xFF);
+			myprintf(" %x", temp);
+		}
+		break;
+		case kCMD08 :
+		myprintf("\nR7 = ");
+		for(i=0; i<LENGTH_R7; i++) {
+			temp = spiSend(0xFF);
+			myprintf(" %x", temp);
+		}
+		break;
+		case kCMD41 :
+		for(i=0; i<LENGTH_R1-1; i++) {
+			temp = spiSend(0xFF);
+			myprintf(" %x", temp);
+		}
+		spiSend(0xFF);
+		break;
+		case kCMD55 :
+		for(i=0; i<LENGTH_R1; i++) {
+			temp = spiSend(0xFF);
+			//myprintf(" %x", temp);
+		}
+		break;
+		default :
+		myprintf("\n Error in CMD");
+	}
+	REG_PORT_OUTSET0 = PORT_PA18;
+	return(temp);
 }
 
 int main(void)
 {
 	UARTInit();
 	spiInit();
-	volatile uint8_t rData;
-	volatile uint8_t sData = 85;
-	while (1) {
-		REG_PORT_OUTCLR0 = PORT_PA18; //initiate transaction by SS_low
-		rData = spiSend( sData );
-		REG_PORT_OUTSET0 = PORT_PA18; //finish transaction by SS_high
-		//myprintf(" %x\n", rData);
-	}
+	
+	initCycles();
+
+
+	myprintf("\nStart Communication");
+	myprintf("\nSending 0x00");
+	spiXchg( CMD00, SIZE_SD_CMD, RxBuffer ); /* reset instruction */
+	//myprintf("\nSending 0x08");
+	//spiXchg( CMD08, SIZE_SD_CMD, RxBuffer );
+
+	myprintf("\nDone");
 }
 
 void UARTInit(void) {
